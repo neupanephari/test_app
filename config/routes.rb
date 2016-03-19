b6cf12544f29ca2c2c9793db5c7c249e11628eff
Rails Udemy Course <neupanephari@gmail.com>
2016-03-19 20:58:39 +0000

    create User table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :comments
+  resources :articles
+  resources :users
   resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -38,8 +42,7 @@ Rails.application.routes.draw do
 
   # Example resource route with more complex sub-resources:
   #   resources :products do
-  #     resources :comments
-  #     resources :sales do
+  #     #     resources :sales do
   #       get 'recent', on: :collection
   #     end
   #   end
